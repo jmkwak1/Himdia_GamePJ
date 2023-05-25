46397793e14590287d77a4b03931bfe9e95be140
jmkwak <84165763+jmkwak1@users.noreply.github.com>
2023-05-25 17:02:32 +0900

    Update tesst.java

--- Hi_GamePJ/src/main/tesst.java
+++ Hi_GamePJ/src/main/tesst.java
@@ -3,5 +3,9 @@ package main;
 public class tesst {
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("123");
 	}
 }
