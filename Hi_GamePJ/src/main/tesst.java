b9b63876bdeece451df16ca00d8194b674a72d6a
hi <hi@DESKTOP-1LTK4GI>
2023-05-25 17:29:11 +0900

    123

--- Hi_GamePJ/src/main/tesst.java
+++ Hi_GamePJ/src/main/tesst.java
@@ -4,6 +4,6 @@ public class tesst {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("asd");
-		System.out.println("123");
+		System.out.println("광일");
 	}
 }
