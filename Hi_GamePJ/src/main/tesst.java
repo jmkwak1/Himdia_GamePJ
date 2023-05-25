9a8142a3a403035ffc00896f85e9cbfb6afa60e1
hii <hii@DESKTOP-67TRPM3>
2023-05-25 16:40:43 +0900

    1

--- Hi_GamePJ/src/main/tesst.java
+++ Hi_GamePJ/src/main/tesst.java
@@ -3,6 +3,8 @@ package main;
 public class tesst {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("test"); 
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
 	}
 }
