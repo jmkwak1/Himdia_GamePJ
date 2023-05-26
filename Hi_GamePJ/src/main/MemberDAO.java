0c369cf6edf72ef7512cffde90780232ffbb88e0
hii <hii@DESKTOP-67TRPM3>
2023-05-26 14:44:42 +0900

    23.05.26/14:44 main 이름 변경 및 추가

---
+++ Hi_GamePJ/src/main/MemberDAO.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDAO {
+	private Connection con;
+	
+	public MemberDAO() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "oracle";
+		String password = "oracle";
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			con = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String login(String id) {
+		String sql = "SELECT pw FROM higame WHERE id=?";
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, id);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				return rs.getString("pw");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void insert(
+			String id, String pw, String name, String email
+			) {
+		String sql = "INSERT INTO higame VALUES(?,?,?,?)";
+		PreparedStatement ps = null;
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, id);
+			ps.setString(2, pw);
+			ps.setString(3, name);
+			ps.setString(4, email);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
