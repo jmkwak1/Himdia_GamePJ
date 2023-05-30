8e8467f5140ee6eed272fde8dd15fc9f516f8140
hi <hi@DESKTOP-SSOAHRP>
2023-05-30 17:24:15 +0900

    2023.05.30/17:24

---
+++ Hi_GamePJ/src/project/GameSettingsDTO.java
@@ -0,0 +1,31 @@
+package project;
+
+public class GameSettingsDTO {
+    private int row;
+    private int col;
+    private int mineCount;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getMineCount() {
+        return mineCount;
+    }
+
+    public void setMineCount(int mineCount) {
+        this.mineCount = mineCount;
+    }
+}
