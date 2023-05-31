de2e45a9ee47f7c552dd9918801b7feeba3b9102
hii <hii@DESKTOP-67TRPM3>
2023-05-31 15:12:08 +0900

    23.05.31/15:11 기존 첫번째 행 로그인 되는거 복구

--- Hi_GamePJ/src/main/MemberDAO.java
+++ Hi_GamePJ/src/main/MemberDAO.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.lang.reflect.Member;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -38,9 +39,7 @@ public class MemberDAO {
 		return null;
 	}
 	
-	public void insert(
-			String id, String pw, String name, String email
-			) {
+	public void insert(String id, String pw, String name, String email) {
 		String sql = "INSERT INTO higame (id, pw, name, email) VALUES(?,?,?,?)";
 		PreparedStatement ps = null;
 		try {
@@ -54,4 +53,5 @@ public class MemberDAO {
 			e.printStackTrace();
 		}
 	}
+
 }
