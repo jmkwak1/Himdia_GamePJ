3e872a7b1ad40391d41d38cd8411c639489ee553
hii <hii@DESKTOP-67TRPM3>
2023-06-02 16:39:26 +0900

    23.06.02/16:38 14:30분껄로 복원

--- Hi_GamePJ/src/main/LoginController.java
+++ Hi_GamePJ/src/main/LoginController.java
@@ -13,68 +13,46 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginController implements Initializable {
-    @FXML
-    private TextField idFld;
-    @FXML
-    private PasswordField pwFld;
-    private LoginService service;
+public class LoginController implements Initializable{
+	@FXML
+	private TextField idFld;
+	@FXML
+	private PasswordField pwFld;
+	private LoginService service;
+	
+	private Opener opener;
+	public void setOpener(Opener opener) {
+		this.opener = opener;
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		service = new LoginService();
+	}
+	
+	// 로그인 버튼을 클릭하면 동작하는 메서드
+	public void loginProc() {
+		service.login(idFld.getText(), pwFld.getText());
+		// 로그인성공이라면 메인화면을 로그인 창이 있는 곳에 메인화면을 실행
+		// 로그인실패라면 아무것도 하지 않는다.
+		System.out.println("로그인 성공여부 : " + Login.getId());
+		if(Login.getId() != null) {
+			opener.mainOpen();
+			
+ 			// opener or mainOpen(); null 어디에 들어가있는 확인
+		}
+	}
+	
+	public void cancelProc() {
+		idFld.clear();
+		pwFld.clear();
+		idFld.requestFocus();
+	}
+	
+	public void regProc() {
+		// 회원가입 화면을 새창으로 실행
+		opener.regOpen();
+	}
+	
 
-    private Opener opener;
-    private Stage primaryStage;
-
-    public void setOpener(Opener opener) {
-        this.opener = opener;
-    }
-
-    public void setPrimaryStage(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        service = new LoginService();
-    }
-
-    // 로그인 버튼을 클릭하면 동작하는 메서드
-    public void loginProc() {
-        service.login(idFld.getText(), pwFld.getText());
-        // 로그인 성공이라면 메인 화면을 실행하고, 로그인 실패라면 아무 작업도 수행하지 않음
-        System.out.println("로그인 성공여부: " + Login.getId());
-        System.out.println("@LoginSerVice_idFld: " + idFld);
-        System.out.println("@LoginSerVice_pwFld: " + pwFld);
-        System.out.println("@LoginSerVice_Login.getId(): " + Login.getId());
-
-        if (Login.getId() != null || opener == null) {
-            if (opener != null) {
-                // opener 객체에 값을 설정하여 메인 화면을 열도록 변경
-                opener.setPrimaryStage(primaryStage);
-                opener.mainOpen();
-                System.out.println("#LoginSerVice_opener: " + opener);
-                System.out.println("#LoginSerVice_idFld: " + idFld);
-                System.out.println("#LoginSerVice_pwFld: " + pwFld);
-                System.out.println("#LoginSerVice_Login.getId(): " + Login.getId());
-            } else {
-                System.out.println("Opener 객체가 초기화되지 않았습니다.");
-                System.out.println("LoginSerVice_opener: " + opener);
-                System.out.println("LoginSerVice_idFld: " + idFld);
-                System.out.println("LoginSerVice_pwFld: " + pwFld);
-                System.out.println("LoginSerVice_Login.getId(): " + Login.getId());
-            }
-        }
-    }
-
-    public void cancelProc() {
-        idFld.clear();
-        pwFld.clear();
-        idFld.requestFocus();
-    }
-
-    public void regProc() {
-        if (opener != null) {
-            opener.regOpen();
-        } else {
-            System.out.println("Opener 객체가 초기화되지 않았습니다.");
-        }
-    }
 }
