311ec6816205d141aef1d90dbe2aaca38848db51
hi <hi@DESKTOP-SSOAHRP>
2023-06-02 14:20:03 +0900

    2023.06.02/14:19 javaopner,opner,logincontroller 수정

--- Hi_GamePJ/src/main/JavaOpener.java
+++ Hi_GamePJ/src/main/JavaOpener.java
@@ -1,4 +1,5 @@
 package main;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,41 +9,77 @@ import javafx.stage.Stage;
 import project.Game;
 import Blackjack.Blackjack;
 import tetris.TetrisGame;
+import javafx.scene.Node;
 
 import java.io.IOException;
 
 public class JavaOpener {
-		
+
+	private Stage primaryStage; // primaryStage 필드 추가
+
+	public void setPrimaryStage(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+	}
 	public void boomOpen(ActionEvent event) {
 		System.out.println("지뢰찾기 시작");
 		try {
-            Stage gameStage = new Stage();
-            Game game = new Game();
-            game.start(gameStage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			Stage gameStage = new Stage();
+			Game game = new Game();
+			game.start(gameStage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void BlackjackOpen(ActionEvent event) {
 		System.out.println("블랙잭 시작");
 		try {
-	        Stage blackjackStage = new Stage();
-	        Blackjack main = new Blackjack();
-	        main.start(blackjackStage);
-	    } catch (Exception e) {
+			Stage blackjackStage = new Stage();
+			Blackjack main = new Blackjack();
+			main.start(blackjackStage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void tetrisOpen(ActionEvent event) {
+		System.out.println("테트리스 시작");
+		try {
+			Stage tetrisStage = new Stage();
+			TetrisGame main = new TetrisGame();
+			main.start(tetrisStage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void boardOpen(ActionEvent event) {
+	    try {
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("board.fxml"));
+	        Parent boardForm = loader.load();
+
+	        Stage boardStage = new Stage();
+	        boardStage.setScene(new Scene(boardForm));
+	        boardStage.setTitle("게시판");
+	        boardStage.show();
+	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
 	}
 	
-	public void tetrisOpen(ActionEvent event) {
-		System.out.println("테트리스 시작");
-		try {
-	        Stage tetrisStage = new Stage();
-	        TetrisGame main = new TetrisGame();
-	        main.start(tetrisStage);
-	    } catch (Exception e) {
+	public void logout(ActionEvent event) {
+	    try {
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(getClass().getResource("loginForm.fxml"));
+	        Parent boardForm = loader.load();
+
+	        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	        primaryStage.setScene(new Scene(boardForm));
+	        primaryStage.setTitle("로그인 화면");
+	        primaryStage.show();
+	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
 	}
+
 }
