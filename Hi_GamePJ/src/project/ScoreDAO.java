2048200d9d2b5510046e43bd1c8e9f505fc61938
hi <hi@DESKTOP-SSOAHRP>
2023-06-09 14:47:12 +0900

    2023.06.09/14:46 project 파일 다시 업로드

--- Hi_GamePJ/src/project/ScoreDAO.java
+++ Hi_GamePJ/src/project/ScoreDAO.java
@@ -1,29 +1,58 @@
- package project;
+package project;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import main.Login;
+import main.MemberDAO;
+import main.MemberDTO;
+
 public class ScoreDAO {
-    private static final String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 데이터베이스 URL
-    private static final String user = "oracle"; // 데이터베이스 사용자 이름
-    private static final String password = "oracle"; // 데이터베이스 비밀번호
+    private Connection connection;
 
-    public static void saveScore(String id, int score) {
-        String sql = "INSERT INTO higame(score) VALUES (?)"; // 쿼리문
+    public ScoreDAO() {
+        // 데이터베이스 연결 설정
+        String url = "jdbc:oracle:thin:@localhost:1521:xe"; // 데이터베이스 URL
+        String user = "oracle"; // 데이터베이스 사용자명
+        String password = "oracle"; // 데이터베이스 비밀번호
 
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-
-            stmt.setInt(1, score);
-            stmt.executeUpdate();
-
-            System.out.println("Score saved to the database.");
-
-        } catch (SQLException e) {
-            System.out.println("Failed to save score to the database.");
+        try {
+            // 드라이버 로드
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            // 데이터베이스 연결
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("데이터베이스 연결 성공");
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            System.out.println("드라이버 로드 오류: " + e.getMessage());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("데이터베이스 연결 오류: " + e.getMessage());
         }
+    }
+
+    public void saveScore(String id, int score) {
+        MemberDAO memberDAO = new MemberDAO();
+        MemberDTO member = memberDAO.login(id);
+
+        try {
+            int newGold = member.getGold() + score; // 기존 gold 값에 score를 더한 새로운 gold 값 계산
+
+            String updateQuery = "UPDATE higame SET gold = ? WHERE id = ?";
+            PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
+            updateStatement.setInt(1, newGold);
+            updateStatement.setString(2, id);
+            updateStatement.executeUpdate();
+
+            System.out.println("데이터베이스에 값이 저장되었습니다.");
+            System.out.println(newGold);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("데이터베이스 오류: " + e.getMessage());
+        }
+         score = 0;
     }
 }
